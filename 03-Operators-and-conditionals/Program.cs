67d9c691af3be8c3064eb000e9ea2327cb34d339
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-27 15:07:56 +0100

    Added Math operators

--- 03-Operators-and-conditionals/Program.cs
+++ 03-Operators-and-conditionals/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int myInt = 5;
 int mySecondInt = 10;
+
+Console.WriteLine(myInt);
+
+myInt++;
+Console.WriteLine(myInt);
+
+myInt += 7;
+Console.WriteLine(myInt);
+
+myInt -= 8;
+Console.WriteLine(myInt);
+
+Console.WriteLine(myInt * mySecondInt);
+Console.WriteLine(mySecondInt / myInt);
+Console.WriteLine(mySecondInt + myInt);
+Console.WriteLine(myInt - mySecondInt);
+
+Console.WriteLine(5+5 * 10);
+Console.WriteLine((5+5) * 10);
+
+Console.WriteLine(Math.Pow(5,4));
+Console.WriteLine(Math.Sqrt(25));
