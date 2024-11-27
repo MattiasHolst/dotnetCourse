569f3dfff891bcfb5ae247214015909ad7cdede2
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-27 11:52:11 +0100

    Added List (and Array) to the datastructures

--- 02-DataStructures/Program.cs
+++ 02-DataStructures/Program.cs
@@ -1,6 +1,16 @@
 ﻿string[] myGroceryArray = ["Cheese", "Milk"];
+//string[] myGroceryArray = new string[2];
 
 myGroceryArray[1] = "Yoghurt";
 
 Console.WriteLine(myGroceryArray[0]);
 Console.WriteLine(myGroceryArray[1]);
+// Console.WriteLine(myGroceryArray[2]); Index out of bounds
+
+// List<string> myGroceryList = new List<string>();
+List<string> myGroceryList = ["Ice Cream", "Coffee"];
+
+Console.WriteLine(myGroceryList[0]);
+Console.WriteLine(myGroceryList[1]);
+//Console.WriteLine(myGroceryList[2]); Index out of range
+
