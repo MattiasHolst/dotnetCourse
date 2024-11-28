e6a2e64807d19dee5503466328b486500c0bba7f
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 13:27:26 +0100

    Added a Computer model

---
+++ 07-Models/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _07_Models;
+
+public class Computer
+{
+    public string Motherboard { get; set; }
+    public int CPUCores { get; set; }
+    public bool HasWifi { get; set; }
+    public bool HasLTE { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public decimal Price { get; set; }
+    public string VideoCard { get; set; }
+
+    public Computer()
+    {
+        VideoCard ??= "";
+        Motherboard ??= "";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Computer myComputer = new()
+        {
+            Motherboard = "Z690",
+            HasWifi = true,
+            HasLTE = false,
+            ReleaseDate = DateTime.Now,
+            Price = 943.87m,
+            CPUCores = 1,
+            VideoCard = "RTX 2060"
+        };
+
+        Console.WriteLine(myComputer.Motherboard);
+        Console.WriteLine(myComputer.HasWifi);
+        Console.WriteLine(myComputer.ReleaseDate);
+        Console.WriteLine(myComputer.VideoCard);
+
+    }
+}
