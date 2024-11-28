e03290a49a373a50a766ddd0c676b23829e16bbd
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 14:33:54 +0100

    Added a Computer model and now able to insert to DB and select from DB

--- 09-Database-Connections/Program.cs
+++ 09-Database-Connections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data;
+using System.Globalization;
+using _09_Database_Connections.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
 
@@ -16,5 +18,59 @@ class Program
         DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
 
         Console.WriteLine(rightNow);
+
+        Computer myComputer = new()
+        {
+            Motherboard = "Z690",
+            HasWifi = true,
+            HasLTE = false,
+            ReleaseDate = DateTime.Now,
+            Price = 943.87m,
+            VideoCard = "RTX 2060"
+        };
+
+        string sql = @$"INSERT INTO TutorialAppSchema.Computer (
+            Motherboard,
+            HasWifi,
+            HasLTE,
+            ReleaseDate,
+            Price,
+            VideoCard
+        ) VALUES (
+            '{myComputer.Motherboard}',
+            '{myComputer.HasWifi}',
+            '{myComputer.HasLTE}',
+            '{myComputer.ReleaseDate}',
+            '{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}',
+            '{myComputer.VideoCard}')";
+
+        Console.WriteLine(sql);
+
+        int result = dbConnection.Execute(sql);
+
+        Console.WriteLine(result);
+
+        string sqlSelect = @"
+        SELECT  
+            Computer.Motherboard,
+            Computer.HasWifi,
+            Computer.HasLTE,
+            Computer.ReleaseDate,
+            Computer.Price,
+            Computer.VideoCard 
+        FROM TutorialAppSchema.Computer";
+
+        IEnumerable<Computer> computers = dbConnection.Query<Computer>(sqlSelect);
+
+        Console.WriteLine("'Motherboard','HasWifi','HasLTE','ReleaseDate','Price', 'VideoCard'");
+        foreach (Computer computer in computers)
+        {
+            Console.WriteLine("'" + computer.Motherboard
+                + "','" + computer.HasWifi
+                + "','" + computer.HasLTE
+                + "','" + computer.ReleaseDate
+                + "','" + computer.Price.ToString("0.00", CultureInfo.InvariantCulture)
+                + "','" + computer.VideoCard + "'");
+        }
     }
 }
