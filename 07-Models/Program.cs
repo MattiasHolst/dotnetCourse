c527dbe1eba528623b630d3ae0908267c16570ac
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 13:29:37 +0100

    Removed constructor and added string values at their properties

--- 07-Models/Program.cs
+++ 07-Models/Program.cs
@@ -2,19 +2,14 @@
 
 public class Computer
 {
-    public string Motherboard { get; set; }
+    public string Motherboard { get; set; } = "";
     public int CPUCores { get; set; }
     public bool HasWifi { get; set; }
     public bool HasLTE { get; set; }
     public DateTime ReleaseDate { get; set; }
     public decimal Price { get; set; }
-    public string VideoCard { get; set; }
+    public string VideoCard { get; set; } = "";
 
-    public Computer()
-    {
-        VideoCard ??= "";
-        Motherboard ??= "";
-    }
 }
 
 class Program
