f5e235d3ac0bfdc00a785e7db9367ca5c66133c9
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 14:04:53 +0100

    Able to connect to the DB

--- 09-Database-Connections/Program.cs
+++ 09-Database-Connections/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace _09_Database_Connections;
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace _09_Database_Connections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true";
+
+        IDbConnection dbConnection = new SqlConnection(connectionString);
+
+        string sqlCommand = "SELECT GETDATE()";
+        DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+        Console.WriteLine(rightNow);
     }
 }
