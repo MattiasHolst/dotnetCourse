bc7e1045976a22c1515f4e94a0c5793b5edcb13d
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 11:08:45 +0100

    added for loops and foreach

--- 05-Loops/Program.cs
+++ 05-Loops/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] intsToCompress = [10, 15, 20, 25, 30, 12, 34];
+
+DateTime startTime = DateTime.Now;
+
+int totalValue = intsToCompress[0] + intsToCompress[1]
++ intsToCompress[2] + intsToCompress[3] + intsToCompress[4]
++ intsToCompress[5] + intsToCompress[6];
+
+Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+Console.WriteLine(totalValue);
+
+//146
+totalValue = 0;
+startTime = DateTime.Now;
+for (int i = 0; i < intsToCompress.Length; i++)
+{
+    totalValue += intsToCompress[i];
+}
+
+Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+Console.WriteLine(totalValue);
+
+totalValue = 0;
+startTime = DateTime.Now;
+foreach (int intForCompression in intsToCompress)
+{
+    totalValue += intForCompression;
+}
+Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+Console.WriteLine(totalValue);
