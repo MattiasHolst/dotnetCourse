bc56413dc2a0d3d890c983bdf13a91e9a79e083e
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 10:47:23 +0100

    Added if statements and switch case

--- 04-Conditional-Statements/Program.cs
+++ 04-Conditional-Statements/Program.cs
@@ -2,8 +2,38 @@
 int myInt = 5;
 int mySecondInt = 10;
 
-if(myInt < mySecondInt){
+if (myInt < mySecondInt)
+{
     myInt += 10;
 }
 
 Console.WriteLine(myInt);
+
+string myCow = "cow";
+string myCapitalizedCow = "Cow";
+
+if (myCow == myCapitalizedCow)
+{
+    Console.WriteLine("Equal");
+}
+else if (myCow == myCapitalizedCow.ToLower())
+{
+    Console.WriteLine("Equal without case sensitivity");
+}
+else
+{
+    Console.WriteLine("Not Equal");
+}
+
+switch (myCow)
+{
+    case "cow":
+        Console.WriteLine("Lowercase");
+        break;
+    case "Cow":
+        Console.WriteLine("Capitalized");
+        break;
+    default:
+        Console.WriteLine("Default ran");
+        break;
+}
