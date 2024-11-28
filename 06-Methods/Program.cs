f6f46d6c90be76dbaf9cb2f336ef6adcc7fc66ea
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-28 11:44:12 +0100

    Added a GetSum method

--- 06-Methods/Program.cs
+++ 06-Methods/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        int[] intsToCompress = [10, 15, 20, 25, 30, 12, 34];
+
+        DateTime startTime = DateTime.Now;
+
+        int totalValue = 0;
+
+
+        totalValue = intsToCompress.Sum();
+        Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+        Console.WriteLine(totalValue);
+
+        startTime = DateTime.Now;
+
+        totalValue = 0;
+        totalValue = GetSum(intsToCompress);
+        Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+        Console.WriteLine(totalValue);
+
+
+    }
+
+
+
+    static private int GetSum(int[] intsToCompress)
+    {
+        int totalValue = 0;
+        foreach (int intForCompression in intsToCompress)
+        {
+            totalValue += intForCompression;
+        }
+        return totalValue;
+    }
+}
+
