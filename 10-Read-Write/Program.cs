2c9bdbd9d1866b44d32b1a7a8f2b5579e63e7266
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-29 10:24:48 +0100

    Init Read & Write lecture

---
+++ 10-Read-Write/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using _10_Read_Write.Models;
+
+namespace _10_Read_Write;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Computer myComputer = new()
+        {
+            Motherboard = "Z690",
+            HasWifi = true,
+            HasLTE = false,
+            ReleaseDate = DateTime.Now,
+            Price = 943.87m,
+            VideoCard = "RTX 2060"
+        };
+
+        string sql = @$"INSERT INTO TutorialAppSchema.Computer (
+            Motherboard,
+            HasWifi,
+            HasLTE,
+            ReleaseDate,
+            Price,
+            VideoCard
+        ) VALUES (
+            '{myComputer.Motherboard}',
+            '{myComputer.HasWifi}',
+            '{myComputer.HasLTE}',
+            '{myComputer.ReleaseDate}',
+            '{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}',
+            '{myComputer.VideoCard}')";
+
+        
+    }
+}
