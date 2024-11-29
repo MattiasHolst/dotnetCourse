3fa0093d5c512f03fd6995536943ec574950576d
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-29 14:51:41 +0100

    Added async method

--- 11-Async-Await/Program.cs
+++ 11-Async-Await/Program.cs
@@ -10,7 +10,28 @@ class Program
             Console.WriteLine("Task 1");
         });
         firstTask.Start();
+
+        Task secondTask = ConsoleAfterDelayAsync("Task 2", 150);
+
+        ConsoleAfterDelay("Delay", 75);
+
+        Task thirdTask = ConsoleAfterDelayAsync("Task 3", 50);
+
+        await secondTask;
         await firstTask;
         Console.WriteLine("After the Task was created");
+        await thirdTask;
+    }
+
+    static void ConsoleAfterDelay(string text, int delayTime)
+    {
+        Thread.Sleep(delayTime);
+        Console.WriteLine(text);
+    }
+
+    static async Task ConsoleAfterDelayAsync(string text, int delayTime)
+    {
+        await Task.Delay(delayTime);
+        Console.WriteLine(text);
     }
 }
