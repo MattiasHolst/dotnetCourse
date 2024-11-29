8ff329dcf897065c67a33e536ee3794124317a84
Mattias Holst <mattias.holst@nexergroup.com>
2024-11-29 11:29:08 +0100

    Added serializer/deserializer to json to be able to read from json file and insert into a Model and DB

--- 10-Read-Write/Program.cs
+++ 10-Read-Write/Program.cs
@@ -4,6 +4,7 @@ using _10_Read_Write.Data;
 using _10_Read_Write.Models;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace _10_Read_Write;
 
@@ -16,20 +17,6 @@ class Program
 
         DataContextDapper dapper = new DataContextDapper(config);
 
-        // string sql = @$"INSERT INTO TutorialAppSchema.Computer (
-        //     Motherboard,
-        //     HasWifi,
-        //     HasLTE,
-        //     ReleaseDate,
-        //     Price,
-        //     VideoCard
-        // ) VALUES (
-        //     '{myComputer.Motherboard}',
-        //     '{myComputer.HasWifi}',
-        //     '{myComputer.HasLTE}',
-        //     '{myComputer.ReleaseDate}',
-        //     '{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}',
-        //     '{myComputer.VideoCard}')";
 
         // File.WriteAllText("log.txt", "\n" + sql + "\n");
 
@@ -43,24 +30,63 @@ class Program
 
         // Console.WriteLine(computersJson);
 
-        // JsonSerializerOptions options = new()
-        // {
-        //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-        // };
-
-        // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
-        
-        IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
-
-
-        if (computers != null)
+        JsonSerializerOptions options = new()
         {
-            foreach (Computer computer in computers)
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+
+
+        IEnumerable<Computer>? computersNewtonsoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+
+        IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+
+
+        if (computersNewtonsoft != null)
+        {
+            foreach (Computer computer in computersNewtonsoft)
             {
-                Console.WriteLine(computer.Motherboard);
+                // Console.WriteLine(computer.Motherboard);
+                string sql = @$"INSERT INTO TutorialAppSchema.Computer (
+                    Motherboard,
+                    HasWifi,
+                    HasLTE,
+                    ReleaseDate,
+                    Price,
+                    VideoCard
+                ) VALUES (
+                    '{EscapeSingleQuote(computer.Motherboard)}',
+                    '{computer.HasWifi}',
+                    '{computer.HasLTE}',
+                    '{computer.ReleaseDate}',
+                    '{computer.Price.ToString("0.00", CultureInfo.InvariantCulture)}',
+                    '{EscapeSingleQuote(computer.VideoCard)}')";
+
+                dapper.ExecuteSql(sql);
             }
         }
 
+        JsonSerializerSettings settings = new()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
 
+        string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonsoft, settings);
+
+        File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+
+        string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
+
+        File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+
+
+
+    }
+
+    static string EscapeSingleQuote(string input)
+    {
+        string output = input.Replace("'", "''");
+
+        return output;
     }
 }
