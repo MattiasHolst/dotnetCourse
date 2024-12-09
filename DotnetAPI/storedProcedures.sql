bbcfe37e0c2179075b982d6c0d86b52d61f7cace
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-09 09:54:08 +0100

    Added stored procedure to delete userinfo

--- DotnetAPI/storedProcedures.sql
+++ DotnetAPI/storedProcedures.sql
@@ -136,3 +136,20 @@ BEGIN
 
         END
 END
+
+USE DotNetCourseDatabase
+GO
+
+CREATE PROCEDURE TutorialAppSchema.spUser_Delete
+    @UserId INT
+AS
+BEGIN
+    DELETE FROM TutorialAppSchema.Users
+    WHERE UserId = @UserId
+
+    DELETE FROM TutorialAppSchema.UserSalary
+    WHERE UserId = @UserId
+
+    DELETE FROM TutorialAppSchema.UserJobInfo
+    WHERE UserId = @UserId
+END
