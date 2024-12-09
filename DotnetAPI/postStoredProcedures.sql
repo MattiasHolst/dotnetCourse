7a5025f71eadd4ae97d9965bdefc5c41d837d3b4
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-09 10:48:49 +0100

    Added a delete procedure to posts

--- DotnetAPI/postStoredProcedures.sql
+++ DotnetAPI/postStoredProcedures.sql
@@ -59,3 +59,16 @@ BEGIN
                 WHERE PostId = @PostId
         END
 END
+
+USE DotNetCourseDatabase
+GO
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPost_Delete
+    @PostId INT
+    , @UserId INT
+AS
+BEGIN
+    DELETE FROM TutorialAppSchema.Posts 
+        WHERE PostId = @PostId
+        AND UserId = @UserId
+END
