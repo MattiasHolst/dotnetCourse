1ba7a5dbabcfb2530321b4cc555302a3767a0189
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-09 09:51:42 +0100

    Updated storedProcedure sql

--- DotnetAPI/storedProcedures.sql
+++ DotnetAPI/storedProcedures.sql
@@ -65,14 +65,20 @@ CREATE OR ALTER PROCEDURE TutorialAppSchema.spUser_Upsert
 	@LastName NVARCHAR(50),
 	@Email NVARCHAR(50),
 	@Gender NVARCHAR(50),
+	@JobTitle NVARCHAR(50),
+	@Department NVARCHAR(50),
+    @Salary DECIMAL(18,4),
 	@Active BIT = 1,
 	@UserId INT = NULL
 AS 
 BEGIN
+
     IF NOT EXISTS(SELECT * FROM TutorialAppSchema.Users WHERE UserID = @UserID)
         BEGIN
         IF NOT EXISTS(SELECT * FROM TutorialAppSchema.Users WHERE Email = @Email)
             BEGIN
+                DECLARE @OutputUserId INT
+
                 INSERT INTO TutorialAppSchema.Users(
                     [FirstName],
                     [LastName],
@@ -86,6 +92,26 @@ BEGIN
                     @Gender,
                     @Active
                 )
+
+                SET @OutputUserId = @@IDENTITY
+
+                INSERT INTO TutorialAppSchema.UserSalary(
+                    [UserId],
+                    [Salary]
+                ) VALUES (
+                    @OutputUserId,
+                    @Salary
+                )
+
+                INSERT INTO TutorialAppSchema.UserJobInfo(
+                    [UserId],
+                    [JobTitle],
+                    [Department]
+                ) VALUES (
+                    @OutputUserId,
+                    @JobTitle,
+                    @Department
+                )
             END
         END
     ELSE
@@ -96,6 +122,15 @@ BEGIN
                     Email = @Email,
                     Gender = @Gender,
                     Active = @Active
+                WHERE UserId = @UserId
+
+            UPDATE TutorialAppSchema.UserSalary
+                SET Salary = @Salary
+                WHERE UserId = @UserId
+
+            UPDATE TutorialAppSchema.UserJobInfo
+                SET JobTitle = @JobTitle,
+                    Department = @Department
                 WHERE UserId = @UserId
 
 
