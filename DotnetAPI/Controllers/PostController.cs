3162af5284e817046d8ffdb2ed5a33c581ec46a6
Mattias Holst <mattias.holst@nexergroup.com>
2024-12-09 16:00:21 +0100

    Updated delete post endpoint to call stored procedure aswell

--- DotnetAPI/Controllers/PostController.cs
+++ DotnetAPI/Controllers/PostController.cs
@@ -68,9 +68,8 @@ namespace DotnetAPI.Controllers
         [HttpDelete("Post/{postId}")]
         public IActionResult DeletePost(int postId)
         {
-            string sql = @"DELETE FROM TutorialAppSchema.Posts 
-            WHERE PostId = " + postId.ToString() +
-            " AND UserId = " + User.FindFirst("userId")?.Value;
+            string sql = @"EXEC TutorialAppSchema.spPost_Delete @PostId=" + postId.ToString() +
+            ", @UserId = " + User.FindFirst("userId")?.Value;
 
             if (_dapper.ExecuteSql(sql)) return Ok();
             throw new Exception("Failed to Delete Post");
